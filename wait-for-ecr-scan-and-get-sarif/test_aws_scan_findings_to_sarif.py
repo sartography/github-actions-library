e56ffec56a18b4ccd6022406d3f4ebbd4510f860
jasquat <jasquat@users.noreply.github.com>
2024-08-19 11:52:54 -0400

    try to upload sarif file without any findings w/ burnettk

--- wait-for-ecr-scan-and-get-sarif/test_aws_scan_findings_to_sarif.py
+++ wait-for-ecr-scan-and-get-sarif/test_aws_scan_findings_to_sarif.py
@@ -36,3 +36,19 @@ def test_convert_to_sarif_enhanced_ecr_scan():
 
     sarif_report = convert_to_sarif(ecr_response)
     assert sarif_report == expected_response
+
+
+def test_convert_to_sarif_when_no_findings():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    sample_file_path = os.path.join(base_dir, "tests/ecr-scan-result-no-findings.json")
+    with open(sample_file_path, "r") as f:
+        ecr_response = json.load(f)
+
+    expected_output_file_path = os.path.join(
+        base_dir, "tests/ecr-scan-result-no-findings-expected-sarif.json"
+    )
+    with open(expected_output_file_path, "r") as f:
+        expected_response = json.load(f)
+
+    sarif_report = convert_to_sarif(ecr_response)
+    assert sarif_report == expected_response
